6f714a405d15c5d737fe17c9f732b99eb65c33e9
Jean-Baptiste Kerbrat <jbkerbrat@spiria.com>
2021-06-16 15:18:58 -0400

    SUP-9559 cast to array

--- app/controllers/camaleon_cms/admin/posts_controller.rb
+++ app/controllers/camaleon_cms/admin/posts_controller.rb
@@ -53,7 +53,7 @@ class CamaleonCms::Admin::PostsController < CamaleonCms::AdminController
     puts('*********')
     puts(@posts.class)
     puts(@posts)
-    @post = @posts.reverse_order
+    @post = @posts.to_a.reverse
     puts(@post_type)
     puts(@posts)
     puts('*********')
