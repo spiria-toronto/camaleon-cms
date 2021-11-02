99ff7bd6bae0ab3b8bca78028497856ead41ad73
Jean-Baptiste Kerbrat <jbkerbrat@spiria.com>
2021-11-01 21:35:57 -0400

    SUP-11271 fix as

--- config/routes/frontend.rb
+++ config/routes/frontend.rb
@@ -79,7 +79,7 @@ Rails.application.routes.draw do
               }
 
               if pt_id == 23
-                get '/article/:post_type_slug' => :post_type, as: "post_type_#{pt_id}", post_type_id: pt_id, constraints: ->(request) {
+                get '/article/:post_type_slug' => :post_type, as: "article", post_type_id: pt_id, constraints: ->(request) {
                   multilingual_segment = PluginRoutes.all_translations("routes.post_types.#{pt_slug}", default: pt_slug)
                   request.params[:post_type_slug].in?(multilingual_segment)
                 }
